c33c8d530fd6d3a95172ea41e6425448485149f4
salongbas1 <thanapon_bas2000@hotmail.com>
2021-01-12 13:09:58 +0700

    bg

--- client/screen/Detail.js
+++ client/screen/Detail.js
@@ -5,7 +5,7 @@ export default function App({navigation}) {
     
       return (
         <SafeAreaView style={{ flex: 1 }}>
-          <Text>well</Text>
+          <Text>wellhhh</Text>
         </SafeAreaView>
       );
 }
